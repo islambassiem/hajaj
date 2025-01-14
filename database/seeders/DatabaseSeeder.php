ba4868ae424d64be72767078a536f656aa4d98a4
Islam Mac <islam@mac.com>
2025-01-14 20:50:56 +0300

    adding city seedrr

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(100)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         $this->call([
             CategorySeeder::class,
+            CitySeeder::class,
             PostSeeder::class,
         ]);
     }
